9104e97d1f198bc4e0816411389dc6f728282c24
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2024-01-03 11:14:38 +0000

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env
-"""
-Create a function def pascal_triangle(n): that returns a list of lists of 
-integers representing the Pascal’s triangle of n:
-
-Returns an empty list if n <= 0
-You can assume n will be always an integer
-
-Expected Output:
-[1]
-[1,1]
-[1,2,1]
-[1,3,3,1]
-[1,4,6,4,1]
-"""
+#!/usr/bin/python3
 
 def pascal_triangle(n):
     triangle = []
--- 0x00-pascal_triangle/README.md
+++ 0x00-pascal_triangle/README.md
@@ -1,2 +1,1 @@
-# 0x00. Pascal's Triangle
-
+# Pascal Triangle
